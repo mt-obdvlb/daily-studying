0c85e7cd84bac10b663595b9b39992dd9fa5f0f9
mt-obdvlb <mtobdvlb@163.com>
2023-10-20 14:13:36 +0800

    1

---
+++ c学习/BMI.c
@@ -0,0 +1,11 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<math.h>
+//
+//int main() {
+//	int weigh, lengh = 0;
+//	float bmi = 0.0f;
+//	scanf("%d %d", &weigh, &lengh);
+//	bmi = weigh / pow((lengh / 100.0), 2);
+//	printf("%.2f", bmi);
+//}
---
+++ c学习/递归1.c
@@ -0,0 +1,17 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//
+//void print(unsigned int a);
+//int main() {
+//	unsigned int a;
+//	scanf("%u", &a);
+//	print(a);
+//	return 0;l	
+//}
+//void print(unsigned int a) {
+//	
+//	if (a > 9) {
+//		print(a/10);
+//	}
+//	printf("%u ", a%10);
+//}
